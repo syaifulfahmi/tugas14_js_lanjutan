bf3f91e589b722f80ebfb9d65351cd98efb9cb00
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-09-21 09:26:51 +0700

    Add files via upload

---
+++ tugas14.js
@@ -0,0 +1,8 @@
+var nilai = 37
+var angkaLahir = 6
+var hasil = (nilai) / (angkaLahir)
+
+console.log(hasil)
+console.log(Math.round(hasil))
+console.log(Math.ceil(hasil))
+console.log(Math.floor(hasil))
